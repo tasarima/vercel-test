2bf4fdbe55f2eb4724eddfa944bf0ba1c156811b
袴田梨紗 <89180837+hks-pj@users.noreply.github.com>
2026-06-19 14:11:52 +0900

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+const gNav = document.querySelector('#gnav');
+
+document.querySelector('#btn-menu').addEventListener('click',function(){
+    if(gNav.classList.contains('open')){
+        gNav.classList.remove('open');
+        gNav.classList.add('close');
+        setTimeout(function(){
+            gNav.classList.remove('close');
+        },300);
+    }else{
+        gNav.classList.add('open');
+    }
+});
